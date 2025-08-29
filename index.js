01d6bdf3759efc3c88e49ba83e2df2a3cff16b01
LMK360 <lmkfail080@gmail.com>
2025-08-29 16:33:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ app.post('/stop', (req, res) => {
 
 // Node version info endpoint
 app.get('/node-version', (req, res) => 
-  exec('node -v', (err, stdout) => 
+  exec('node -v', (err, stdout) => {
     if (err) return res.status(500).json({ error: 'Failed to get Node.js version' });
     res.json( nodeVersion: stdout.trim() );
       );
