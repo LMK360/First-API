b0b3285bebe2b090ed00d321a79019aceda7201a
LMK360 <lmkfail080@gmail.com>
2025-08-29 17:15:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -153,5 +153,5 @@ app.get('/node-version', (req, res) => {
     };
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log({`Server running on port ${PORT}`});
 });
