647483b093ee35c47d578588efda1303ef77bee0
LMK360 <lmkfail080@gmail.com>
2025-08-30 12:53:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const tempDir = path.join(__dirname, 'temp');
 if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir);
